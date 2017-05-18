9a3f617d145accd39ebf95a9855aefdb565c0683
Aleks Obrazcova <aobrazcova@gmail.com>
2017-05-18 17:33:48 +0100

    added decision trees scripts

--- SourceFiles/ud120-projects/decision_tree/dt_author_id.py
+++ SourceFiles/ud120-projects/decision_tree/dt_author_id.py
@@ -29,3 +29,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 
 
+from sklearn import tree
+def decisionTreeSampler(min_samples_split, features_train, features_test, labels_train, labels_test):
+    t0 = time()
+    clf = tree.DecisionTreeClassifier(min_samples_split= min_samples_split)
+    clf = clf.fit(features_train, labels_train)
+    print("training time:", round(time()-t0, 3), "s")
+
+    t0 = time()
+    pred = clf.predict(features_test)
+    print("prediction time:", round(time()-t0, 3), "s")
+
+    from sklearn.metrics import accuracy_score
+    print("Prediction accuracy: ", accuracy_score(labels_test, pred))
+    print("Chris's emails: ", numpy.sum(pred == 1))
+
+min_samples_split = 2
+decisionTreeSampler(min_samples_split, features_train, features_test , labels_train, labels_test)
--- SourceFiles/ud120-projects/svm/svm_author_id.py
+++ SourceFiles/ud120-projects/svm/svm_author_id.py
@@ -26,5 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn import svm
+def svmModeler(C, kernelType, features_train, features_test, labels_train, labels_test):
+    clf = svm.SVC(kernel= kernelType, C= C)
+    t0 = time()
+    clf.fit(features_train, labels_train ) 
+    print("training time:", round(time()-t0, 3), "s")
 
+    t0 = time()
+    pred = clf.predict(features_test)
+    print(pred)
+    print("prediction time:", round(time()-t0, 3), "s")
 
+    from sklearn.metrics import accuracy_score
+    print("Prediction accuracy: ", accuracy_score(labels_test, pred))
+
+C = 6000.0  # SVM regularization parameter
+svmModeler(C, "rbf", features_train, features_test , labels_train, labels_test)
+
+print("Chris's emails: ", numpy.sum(pred == 1))
